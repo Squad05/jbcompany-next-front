11a7c1fb2cbf5016772035288b59da28e2536eb2
1Jeferson <jefersonnanet.95@gmail.com>
2023-11-27 14:57:36 -0300

    Feat: inserindo o backend

---
+++ backend/src/main/java/com/jobdelas/JobdelasApplication.java
@@ -0,0 +1,13 @@
+package com.jobdelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobdelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobdelasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jobdelas/JobdelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobdelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobdelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
