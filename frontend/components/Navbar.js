ba49084c43eb250d651ca80a3e8c1262cd38d899
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-28 15:53:39 -0300

    Feat: Estrutura html navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,6 +1,13 @@
 import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
+  const scroolParaSessao = (id) => {
+    const sessao = document.getElementById(id);
+    if (sessao) {
+      sessao.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav
       className={`navbar navbar-expand-lg fixed-top ${styles.header}`}
@@ -24,10 +31,21 @@ export default function Navbar() {
       </button>
       <div className={`collapse navbar-collapse ${styles.menu}`} id="navbarNav">
         <ul className={`navbar-nav ${styles.containerMenu}`}>
-          <li> Teste</li>
-          <li> Teste</li>
-          <li> Teste</li>
-          <li> Teste</li>
+          <li>
+            <a href="#sobre" onClick={() => scroolParaSessao("section1")}>
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a href="#equipe" onClick={() => scroolParaSessao("equipe")}>
+              Equipe
+            </a>
+          </li>
+          <li>
+            <a href="#faq" onClick={() => scroolParaSessao("faq")}>
+              Faq
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
