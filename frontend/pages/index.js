851d49cf1723f24ad2e16efda30585e5a01b1f22
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-28 15:57:29 -0300

    Feat: Defini estrutura page index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,23 @@
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "@/components/Footer";
+import Cards from "@/components/Cards";
+import Carrossel from "@/components/Carrossel";
+import Faq from "@/components/Faq";
+import Equipe from "@/components/Equipe";
 
 export default function Home() {
-  return <Navbar />;
+  return (
+    <div>
+      <Head>
+        <title> Jb Company</title>
+      </Head>
+      <Navbar />
+      <Carrossel />
+      <Cards />
+      <Faq />
+      <Equipe />
+      <Footer />
+    </div>
+  );
 }
