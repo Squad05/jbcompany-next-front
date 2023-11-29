fc892e9b788883eba476c44171df4285b3dfa0d6
1Jeferson <jefersonnanet.95@gmail.com>
2023-11-29 14:48:29 -0300

    Feat: Component de card

--- frontend/components/Cards.js
+++ frontend/components/Cards.js
@@ -1,3 +1,15 @@
 import styles from "../styles/Cards.module.css";
 
-export default function Cards() {}
+export default function Cards() {
+    return (
+        <div className="corpo-card">
+            <div className="img-card">
+                <img src="" alt="img card" />
+            </div>
+            <div className="card-info">
+                <h2>Titulo card</h2>
+                <p>Descrição do card</p>
+            </div>
+        </div>
+    );
+}
