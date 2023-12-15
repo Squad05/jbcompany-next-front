53301420f02cc35521773b3bc493ae14c939d226
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-12-15 10:23:16 -0300

    Feat: Service do curso

--- backend/api/src/main/java/com/api/jbcompany/api/repository/VagasRepository.java
+++ backend/api/src/main/java/com/api/jbcompany/api/repository/VagasRepository.java
@@ -1,7 +1,6 @@
 package com.api.jbcompany.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.api.jbcompany.api.model.Vagas;
 
 public interface VagasRepository extends JpaRepository<Vagas, Long> {
