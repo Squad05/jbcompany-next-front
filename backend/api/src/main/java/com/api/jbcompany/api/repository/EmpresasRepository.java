17885e1774399abf59e2184aa1731b9687378e60
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-12-15 10:14:35 -0300

    Feat: Repository Cursos , Empresas

---
+++ backend/api/src/main/java/com/api/jbcompany/api/repository/EmpresasRepository.java
@@ -0,0 +1,9 @@
+package com.api.jbcompany.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.jbcompany.api.model.Empresas;
+
+public interface EmpresasRepository extends JpaRepository<Empresas, Long> {
+
+}
