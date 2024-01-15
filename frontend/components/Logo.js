f0b55f7a378d6f5f2ce59d8c1d441a5ce40256d2
1Jeferson <jefersonnanet.95@gmail.com>
2024-01-15 14:35:32 -0300

    Fix: arrumando o caminho do logo

--- frontend/components/Logo.js
+++ frontend/components/Logo.js
@@ -2,6 +2,10 @@ import styles from "../styles/Logo.module.css";
 
 export default function Logo() {
   return (
-    <img className={styles.estilologo} src="imagens/logojbcompany.png"></img>
+    <img
+      className={styles.estilologo}
+      src="../imagens/logojbcompany.png"
+      alt="logo jobdelas"
+    ></img>
   );
 }
