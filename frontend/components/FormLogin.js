c550f880818352e2f5f35256932711821e9588d9
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-16 14:20:08 -0300

    Login

---
+++ frontend/components/FormLogin.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+
+import React, { useState } from "react";
+import styles from "../styles/FormLandingPage.module.css";
+
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Button,
+  Typography,
+} from "@mui/material";
+
+export default function FormLogin() {
+  const [senha, setSenha] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMensagem, setErrorMensagem] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const dados = {
+      email,
+      senha,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://jbcompanyapi.onrender.com/auth/logar",
+        dados
+      );
+
+      console.log("response" + response);
+
+      if (response.data) {
+        const { token } = response.data;
+
+        localStorage.setItem("token", token);
+
+        console.log("token " + token);
+      } else {
+        setErrorMensagem("Email ou senha invalidos");
+      }
+    } catch (erro) {
+      console.error("Erro ao enviar requisição:", erro.message);
+    }
+  };
+
+  return (
+    <form className={styles.formulario_container} onSubmit={handleSubmit}>
+      <Typography variant="h1" component="h1">
+        Login
+      </Typography>
+
+      <FormControl fullWidth>
+        <InputLabel htmlFor="email">Email</InputLabel>
+        <Input
+          id="email"
+          aria-describedby="Digite seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel htmlFor="senha">Senha</InputLabel>
+        <Input
+          id="senha"
+          type="password"
+          aria-describedby="Digite sua senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          required
+        />
+      </FormControl>
+
+      {errorMensagem && (
+        <Typography variant="body2" color="error" gutterBottom>
+          {errorMensagem}
+        </Typography>
+      )}
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ marginTop: 20 }}
+        type="submit"
+      >
+        Enviar
+      </Button>
+    </form>
+  );
+}
--- frontend/pages/auth/logar.js
+++ frontend/pages/auth/logar.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import FormLogin from "@/components/FormLogin";
 
 export default function Logar() {
   return (
@@ -6,6 +7,7 @@ export default function Logar() {
       <Head>
         <title> Jb Company - Login </title>
       </Head>
+      <FormLogin />
     </div>
   );
 }
