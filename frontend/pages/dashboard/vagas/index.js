a77e2051a8e9988a904a52ebeb7a1399505b38a2
MeDourado <mariaeduarda.s.dourado@gmail.com>
2024-01-18 13:55:17 -0300

    Service email token

--- frontend/pages/dashboard/vagas/index.js
+++ frontend/pages/dashboard/vagas/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import VagaCardList from "@/components/VagasCard";
 import Menu from "@/components/NavDash";
-
+import { extrairIdDoToken } from "@/services/auth/EmailToken";
 
 export default function Vagas() {
   return (
