15b34e30f525c58b187abba14e4a8df0c26d0a43
1Jeferson <jefersonnanet.95@gmail.com>
2024-01-20 10:07:45 -0300

    Feat: Pegando as cinco vagas do usuario e listando na tabela

--- frontend/components/TabelaUltimasVagasLancadas.js
+++ frontend/components/TabelaUltimasVagasLancadas.js
@@ -1,31 +1,50 @@
-import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import {
-  randomCreatedDate,
-  randomTraderName,
-} from "@mui/x-data-grid-generator";
-
+import React, { useEffect, useState } from "react";
+import VagaService from "@/services/VagaService";
 import styles from "../styles/Tabela_Ultimos_Lancamentos_Home.module.css";
+import { extrairEmailDoToken } from "@/services/auth/EmailToken";
 
 export default function TabelaUltimasVagasLancadas() {
+  const [vagas, setVagas] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const userToken = localStorage.getItem("token");
+        const userEmail = extrairEmailDoToken(userToken);
+
+        const vagasDoUsuario = await VagaService.listarVagasPorEmpresa(
+          userEmail,
+          userToken
+        );
+
+        const ultimasCincoVagas = vagasDoUsuario.slice(0, 5);
+        setVagas(ultimasCincoVagas);
+      } catch (error) {
+        console.error("Erro ao obter vagas do usuário:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <table className={styles.estilo_tabela}>
         <thead>
           <tr>
-            <th>Projetos</th>
-            <th>Categoria</th>
-            <th>Inscritas</th>
-            <th>Data da Postagem</th>
+            <th>Descrição</th>
+            <th>Localização</th>
+            <th>Função</th>
+            <th>Salário</th>
           </tr>
         </thead>
         <tbody>
-          {rows.map((row) => (
-            <tr key={row.id}>
-              <td>{row.name}</td>
-              <td>{row.category}</td>
-              <td>{row.inscritas}</td>
-              <td>{row.dataPostagem}</td>
+          {vagas.map((vaga) => (
+            <tr key={vaga.id}>
+              <td>{vaga.descricao}</td>
+              <td>{vaga.localizacao}</td>
+              <td>{vaga.funcao}</td>
+              <td>{vaga.salario}</td>
             </tr>
           ))}
         </tbody>
@@ -33,41 +52,3 @@ export default function TabelaUltimasVagasLancadas() {
     </div>
   );
 }
-
-const rows = [
-  {
-    id: 1,
-    name: randomTraderName(),
-    category: "Productivity",
-    inscritas: 18,
-    dataPostagem: "randomCreatedDate()",
-  },
-  {
-    id: 2,
-    name: randomTraderName(),
-    category: "Active tasks",
-    inscritas: 28,
-    dataPostagem: "randomCreatedDate()",
-  },
-  {
-    id: 3,
-    name: randomTraderName(),
-    category: "Teams",
-    inscritas: 1,
-    dataPostagem: "randomCreatedDate()",
-  },
-  {
-    id: 4,
-    name: randomTraderName(),
-    category: "Completed",
-    inscritas: 2,
-    dataPostagem: "randomCreatedDate()",
-  },
-  {
-    id: 5,
-    name: randomTraderName(),
-    category: "Completed",
-    inscritas: 8,
-    dataPostagem: "randomCreatedDate",
-  },
-];
