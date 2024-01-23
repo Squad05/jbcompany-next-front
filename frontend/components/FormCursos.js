43174cefa5bad52188d0ff32b1201d35cd5ea04b
1Jeferson <jefersonnanet.95@gmail.com>
2024-01-23 16:26:14 -0300

    Fix: localstorage

--- frontend/components/FormCursos.js
+++ frontend/components/FormCursos.js
@@ -15,7 +15,7 @@ export default function FormularioCursos() {
   const [descricao, setDescricao] = useState("");
   const [duracao, setDuracao] = useState("");
 
-  const token = localStorage.getItem("token");
+  const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
   const router = useRouter();
 
   const handleSubmit = async (e) => {
